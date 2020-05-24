8d2fb1c25c6102260cefb73256bc05cf7b677107
Joe Reddie <joee945@hotmail.co.uk>
2020-05-24 12:47:59 +0100

    remove conditional dependency

--- src/reducer/middleware.js
+++ src/reducer/middleware.js
@@ -1,51 +1,49 @@
-import match from 'conditional-expression';
 import * as actionTypes from '../actionTypes';
 import { getSearchUrl, getIdUrl, sanitizeSearchValue } from './searchUtils';
 
-const applyMiddleware = (dispatch) => (action) =>
-	dispatch(action) ||
-	match(action.type)
-		.equals(actionTypes.TRIGGER_ACTION)
-		.then(() => {
-			const {
-				payload: { searchValue, cache }
-			} = action;
-			const sanitizedSearchValue = sanitizeSearchValue(searchValue);
-			const searchUrl = getSearchUrl(searchValue);
+const applyMiddleware = (dispatch) => (action) => {
+	if (action.type === actionTypes.TRIGGER_ACTION) {
+		const {
+			payload: { searchValue, cache }
+		} = action;
+		const sanitizedSearchValue = sanitizeSearchValue(searchValue);
+		const searchUrl = getSearchUrl(searchValue);
+		dispatch({
+			type: actionTypes.SUBMIT_SEARCH,
+			payload: { movies: [], searchValue: sanitizedSearchValue, cache, response: { error: false, loading: true } }
+		});
+		if (sanitizedSearchValue in cache) {
+			// If we've searched it already just load that
 			dispatch({
 				type: actionTypes.SUBMIT_SEARCH,
-				payload: { movies: [], searchValue: sanitizedSearchValue, cache, response: { error: false, loading: true } }
+				payload: {
+					movies: cache[sanitizedSearchValue],
+					searchValue: sanitizedSearchValue,
+					cache,
+					response: { error: false, loading: false }
+				}
 			});
-			if (sanitizedSearchValue in cache) {
-				// If we've searched it already just load that
-				dispatch({
-					type: actionTypes.SUBMIT_SEARCH,
-					payload: {
-						movies: cache[sanitizedSearchValue],
-						searchValue: sanitizedSearchValue,
-						cache,
-						response: { error: false, loading: false }
-					}
-				});
-			} else {
-				fetch(searchUrl)
-					.then((serverResponse) => serverResponse.json())
-					.then((serverResponseJson) => serverResponseJson.Search.map((movie) => fetch(getIdUrl(movie.imdbID))))
-					.then((requests) => Promise.all(requests))
-					.then((responses) => Promise.all(responses.map((response) => response.json())))
-					.then((movies) => {
-						dispatch({
-							type: actionTypes.SUBMIT_SEARCH,
-							payload: { movies, searchValue: sanitizedSearchValue, cache, response: { error: false, loading: false } }
-						});
+		} else {
+			fetch(searchUrl)
+				.then((serverResponse) => serverResponse.json())
+				.then((serverResponseJson) => serverResponseJson.Search.map((movie) => fetch(getIdUrl(movie.imdbID))))
+				.then((requests) => Promise.all(requests))
+				.then((responses) => Promise.all(responses.map((response) => response.json())))
+				.then((movies) => {
+					dispatch({
+						type: actionTypes.SUBMIT_SEARCH,
+						payload: { movies, searchValue: sanitizedSearchValue, cache, response: { error: false, loading: false } }
+					});
+				})
+				.catch(() =>
+					dispatch({
+						type: actionTypes.SUBMIT_SEARCH,
+						payload: { movies: [], searchValue: sanitizedSearchValue, cache, response: { error: true, loading: false } }
 					})
-					.catch(() =>
-						dispatch({
-							type: actionTypes.SUBMIT_SEARCH,
-							payload: { movies: [], searchValue: sanitizedSearchValue, cache, response: { error: true, loading: false } }
-						})
-					);
-			}
-		})
-		.else(null);
+				);
+		}
+	} else {
+		dispatch(action);
+	}
+};
 export default applyMiddleware;
