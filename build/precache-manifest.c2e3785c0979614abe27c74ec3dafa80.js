056a3c460bc4300d8a6deecb4ccb040beb5a86c5
Joe Reddie <joee945@hotmail.co.uk>
2020-05-24 15:39:05 +0100

    add the build folder

---
+++ build/precache-manifest.c2e3785c0979614abe27c74ec3dafa80.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99aa78228bf063c4e2ad04ed16c5b171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ca57b3b26d61b9e5cb8",
+    "url": "/static/css/main.074b6c8b.chunk.css"
+  },
+  {
+    "revision": "d95ed809467fc04aa66e",
+    "url": "/static/js/2.10c5b98c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10c5b98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca57b3b26d61b9e5cb8",
+    "url": "/static/js/main.02c72b15.chunk.js"
+  },
+  {
+    "revision": "d366470a175b37fbffcc",
+    "url": "/static/js/runtime-main.6bd79009.js"
+  },
+  {
+    "revision": "68abfa647b3bc6bd7ffd17f64fcb3162",
+    "url": "/static/media/loading.68abfa64.svg"
+  },
+  {
+    "revision": "e99c211b794fac010e4df4681e34d5ab",
+    "url": "/static/media/noImage.e99c211b.png"
+  },
+  {
+    "revision": "c5a608c061a2941cf8ac4d6db34fd71e",
+    "url": "/static/media/search.c5a608c0.svg"
+  }
+]);
