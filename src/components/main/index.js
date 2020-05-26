a4e1e22c94eadf16c421face9ac92353e8a6e6ce
Joe Reddie <joee945@hotmail.co.uk>
2020-05-26 11:32:23 +0100

    check when at the bottom of the page

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Cards from './cards';
 import './main.css';
 import Loading from './loading.svg';
@@ -19,6 +19,15 @@ const Main = () => {
 			</object>
 		);
 	}
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+				console.log("you're at the bottom of the page");
+				//show loading spinner and make fetch request to api
+			}
+		});
+	}, []);
+
 	return (
 		<main>
 			{loadingIcon}
